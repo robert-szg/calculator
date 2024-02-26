b9ef643c61a10f8fd00eb40a64099c5e814deaee
robert-szg <155324734+robert-szg@users.noreply.github.com>
2024-02-26 08:38:33 -0300

    Build 4 basic math operations
    Add basic functionality to the calculator

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function add(num1, num2){
+    return parseInt(num1) + parseInt(num2);
+}
+
+function subtract(num1, num2){
+    return parseInt(num1) - parseInt(num2);
+}
+
+function multiply(num1, num2){
+    return parseInt(num1) * parseInt(num2);
+}
+
+function divide(num1, num2){
+    return parseInt(num1) / parseInt(num2);
+}
